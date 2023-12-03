1b8fde91651ca2d9cd5c3ca3f41ccb4664e68c5b
kanseiishikawa <k.ishikw@gmail.com>
2023-12-03 07:58:09 -0500

    fix test

--- main.sh
+++ main.sh
@@ -3,8 +3,9 @@ file_list=`cat $dir/list.txt`
 write_file_name="${dir}/name.py"
 tag=''
 prod_check='test'
+core='6'
 
-while getopts ts-: opt; do
+while getopts cts-: opt; do
     optarg="${!OPTIND}"
     [[ "$opt" = - ]] && opt="-${OPTARG}"
     
@@ -17,6 +18,10 @@ while getopts ts-: opt; do
             tag="${optarg}"
             shift
             ;;
+        c|-core)
+            core="${optarg}"
+            shift
+            ;;
     esac
 done
 
@@ -25,12 +30,11 @@ if [ -z $tag ]; then
     echo '2: data update and learn'
     echo '3: learn'
     echo '4: optuna learn'
-    echo '5: simulation'
 
     read -p "Enter 1,2,3,4,5 > " tag
 fi
 
-if [ !$tag = '1' ] && [ !$tag = '2' ] && [ !$tag = '3' ] && [ !$tag = '4' ] && [ !$tag = '5' ]; then
+if [ !$tag = '1' ] && [ !$tag = '2' ] && [ !$tag = '3' ] && [ !$tag = '4' ]; then
     echo "Wrong number"
     exit 1
 fi
@@ -50,7 +54,7 @@ cp -r $dir data_analyze/
 cp -r $dir learn/
 
 if [ $tag = "2" ]; then
-    mpiexec -n 3 python main.py -u True
+    mpiexec -n ${core} python main.py -u True -l True
 fi
 
 if [ $tag = "3" ]; then
@@ -59,10 +63,6 @@ fi
 
 if [ $tag = "4" ]; then
     python main.py -o True -s "${prod_check}"
-fi
-
-if [ $tag = "5" ]; then
-    python main.py
 fi
 
 rm -rf storage
