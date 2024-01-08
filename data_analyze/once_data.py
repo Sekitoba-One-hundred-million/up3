2b4c23fe441f57c41f8d2152ead2db7dcb99eae9
kansei <kansei@kansei.local>
2024-01-09 02:10:04 +0900

    update

--- data_analyze/once_data.py
+++ data_analyze/once_data.py
@@ -199,8 +199,7 @@ class OnceData:
         two_popular_odds = -1
         
         for horce_id in self.race_data[k].keys():
-            current_data, past_data = lib.race_check( self.horce_data[horce_id],
-                                                     year, day, num, race_place_num )#今回と過去のデータに分ける
+            current_data, past_data = lib.race_check( self.horce_data[horce_id], self.race_day[race_id] )
             cd = lib.current_data( current_data )
             pd = lib.past_data( past_data, current_data )
 
@@ -489,8 +488,7 @@ class OnceData:
         ave_past_min_last_horce_body = lib.average( current_race_data[data_name.past_min_last_horce_body] )
 
         for count, horce_id in enumerate( horce_id_list ):
-            current_data, past_data = lib.race_check( self.horce_data[horce_id],
-                                                     year, day, num, race_place_num )#今回と過去のデータに分ける
+            current_data, past_data = lib.race_check( self.horce_data[horce_id], self.race_day[race_id] )
             cd = lib.current_data( current_data )
             pd = lib.past_data( past_data, current_data )
 
