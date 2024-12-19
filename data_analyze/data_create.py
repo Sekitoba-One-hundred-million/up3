f4e14395cdfe2027b32d1bfa5bf664ed0cb99c73
kanseiishikawa <k.ishikw@gmail.com>
2024-12-19 10:06:54 -0500

    update

--- data_analyze/data_create.py
+++ data_analyze/data_create.py
@@ -61,7 +61,6 @@ def main( update = False ):
         for i in range( 1, size ):
             file_name = comm.recv( source = i, tag = 2 )
             instance = dm.pickle_load( file_name )
-            dm.pickle_delete( file_name )
             result["simu"].update( instance["simu"] )
 
             for k in instance["data"].keys():
